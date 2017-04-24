f1db2a895ea048e6afc432c48569b36723c8133a
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-04-24 16:43:47 +0700

    add gaze button

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # react-vr-gaze-button
-👀 Gaze for {duration} to click the button! 
+
+> 👀 Extension of ReactVR's `VrButton`. Gaze for {duration} ms to click the button!
+
+# Usage
+
+1. Install
+
+```bash
+npm i --save react-vr-gaze-button
+```
+2. Import to your file add pass it `onClick` (function) and `duration` (number, ms) as props. Also pass it a function as the `children` with 1 argument (time remaining in ms) that returns a component.
+
+```js
+import React from 'react'
+import {Text} from 'react-vr'
+import GazeButton from 'react-vr-gaze-button'
+
+class Example extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      buttonIsClicked: false
+    }
+  }
+
+  render () {
+    const {buttonIsClicked} = this.state
+    return (
+      <GazeButton onClick={() => this.setState({buttonIsClicked: true})} duration={2000}>
+          {time => (
+            <Text>
+              {buttonIsClicked ? 'You have clicked me' `Gaze me for ${time} milliseconds`}
+            </Text>
+          )}
+      </GazeButton>
+    )
+  }
+}  
+```
+
+# License
+
+MIT. See [LICENSE](./LICENSE)
---
+++ src/index.js
@@ -0,0 +1,78 @@
+const React = require("react");
+const { View, VrButton } = require("react-vr");
+
+module.exports = class GazeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beingLookedAt: false,
+      remainingTime: 0
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleExit = this.handleExit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.clearCountdown = this.clearCountdown.bind(this);
+    this.resetRemainingTime = this.resetRemainingTime.bind(this);
+  }
+  componentDidMount() {
+    this.resetRemainingTime();
+  }
+
+  componentWillUnmount() {
+    clearCountdown();
+  }
+
+  resetRemainingTime() {
+    const { duration } = this.props;
+    this.setState({ remainingTime: duration });
+  }
+
+  handleEnter() {
+    const { duration, onClick } = this.props;
+    this.gazeStart = Date.now();
+    const endTime = this.gazeStart + duration;
+    this.setState({ beingLookedAt: true });
+    this.countdown = setInterval(
+      () => {
+        const { beingLookedAt } = this.state;
+        const currentTime = Date.now();
+        if (!beingLookedAt) {
+          this.clearCountdown();
+        } else if (currentTime > endTime) {
+          this.handleClick();
+        } else {
+          this.setState({ remainingTime: endTime - currentTime });
+        }
+      },
+      16
+    );
+  }
+
+  handleExit() {
+    this.setState({ beingLookedAt: false });
+  }
+
+  clearCountdown() {
+    const { duration } = this.props;
+    clearInterval(this.countdown);
+    this.setState({ remainingTime: duration });
+  }
+
+  handleClick() {
+    const { onClick } = this.props;
+    this.clearCountdown();
+    onClick();
+  }
+
+  render() {
+    const { onClick, children } = this.props;
+    const { remainingTime } = this.state;
+    return (
+      <View onEnter={this.handleEnter} onExit={this.handleExit}>
+        <VrButton onClick={this.handleClick}>
+          {children(remainingTime)}
+        </VrButton>
+      </View>
+    );
+  }
+};
